356000c8cb5c8e91f0ecd133e05acf86755be47b
aminabdo01 <aminabdo889@gmail.com>
2022-03-05 01:43:10 +0200

    March 5, 2022 1:43 AM

--- Day 01/main.py
+++ Day 01/main.py
@@ -2,4 +2,5 @@ print("Day 1 - String Manipulation")
 print("String Concatenation is done with the \"+\" sign.")
 print('e.g. print("Hello " + " world")')
 print("New lines can be created with a backslash and n.")
+#print the numer of characteres in a name (String) Below Code:
 print(len(input ("What Is Your Name? \n")))

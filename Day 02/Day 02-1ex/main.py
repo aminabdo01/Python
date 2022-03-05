b73843f1f9237eab8139d19625fd06e91d294d21
aminabdo01 <aminabdo889@gmail.com>
2022-03-06 00:02:53 +0200

    March 6, 2022 12:02 AM

--- Day 02/Day 02-1ex/main.py
+++ Day 02/Day 02-1ex/main.py
@@ -4,4 +4,4 @@ two_digit_number = input("Type a two digit number: ")
 
 ####################################
 #Write your code below this line 👇
-print((two_digit_number[0]) +" + " + (two_digit_number[1]) + "=" + str(int(two_digit_number[0] + two_digit_number[1])))
+print((two_digit_number[0]) +" + " + (two_digit_number[1]) + " = " + str(int(two_digit_number[0]) + int(two_digit_number[1])))

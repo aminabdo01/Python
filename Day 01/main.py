357f2f2f2a93450c00ac4cb23ad672ad3ff4e0d7
aminabdo01 <aminabdo889@gmail.com>
2022-03-05 02:06:19 +0200

    March 5, 2022 2:06 AM

--- Day 01/main.py
+++ Day 01/main.py
@@ -4,3 +4,4 @@ print('e.g. print("Hello " + " world")')
 print("New lines can be created with a backslash and n.")
 #print the numer of characteres in a name (String) Below Code: WE USE len() to count things
 print(len(input ("What Is Your Name? \n")))
+se

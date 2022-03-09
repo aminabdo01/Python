371085fc9ca6cb5d7e4e503cd7206756fbd948fc
aminabdo01 <aminabdo889@gmail.com>
2022-03-09 12:53:04 +0200

    March 9, 2022 12:53 PM

--- Day 02/Day 02-3ex/main.py
+++ Day 02/Day 02-3ex/main.py
@@ -7,4 +7,4 @@ Days_left = (365 * 90) - (int(age) * 365)
 Weeks_left = (52 * 90) - (int(age) * 52) 
 Months_left = (12 * 90) - (int(age) * 12)
 
-print(f"You have {Days_left} days, {Weeks_left} weeks, and {Months_left} months left.")
+print("You have {Days_left} days, {Weeks_left} weeks, and {Months_left} months left.")

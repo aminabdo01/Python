32e0eeee2ffccaa3d48c26be6dc46e915c6f143b
aminabdo01 <aminabdo889@gmail.com>
2022-03-09 12:50:00 +0200

    March 9, 2022 12:50 PM

--- Day 02/Day 02-3ex/main.py
+++ Day 02/Day 02-3ex/main.py
@@ -3,3 +3,8 @@ age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+Days_left = (365 * 90) - (age * 365) 
+Weeks_left = (52 * 90) - (age * 52) 
+Months_left = (12 * 90) - (age * 12)
+
+print(f"You have {Days_left} days, {Weeks_left} weeks, and {Months_left} months left.")
